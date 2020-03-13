11a5213c8a4bf3e895d5d305340bb9c61eb99393
pgcementina <trishcmntn@gmail.com>
2020-03-13 13:42:08 -0700

    for exercise 2

--- exercise1.py
+++ exercise1.py
@@ -1,5 +1,9 @@
-hours = float(input("Enter hours: "))
-rate = float(input("Enter rate: "))
+try:
+    hours = float(input("Enter hours: "))
+    rate = float(input("Enter rate: "))
+except:
+    print("Error, please enter numeric input!")
+    quit()
 
 if hours > 40:
     pay = (hours - 40)*rate*1.5 + 40 * rate
